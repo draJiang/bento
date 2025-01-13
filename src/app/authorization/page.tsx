06b3f30a05f1cad0000aa5a68909d635e7718554
jiang <jiang@jiangdeMac-mini.local>
2025-01-14 01:34:45 +0800

    Fix deployment errors.

--- src/app/authorization/page.tsx
+++ src/app/authorization/page.tsx
@@ -1,24 +1,24 @@
 'use client'
 
-import { useEffect,Suspense } from 'react';
+import { useEffect, Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 
 export default function Authorization() {
     const searchParams = useSearchParams();
     const code = searchParams.get('code'); // Replace 'yourParamName' with the actual parameter name you want to retrieve
-    
-    useEffect(()=>{
-        
+
+    useEffect(() => {
+
         console.log(code); // You can use this value as needed
         if (code) {
             window.open(`bento://notion-authorization?${code}`, '_blank'); // Replace with the actual URL you want to open
         }
 
-    },[code])
+    }, [code])
 
     return (
         <Suspense fallback={<div>loading...</div>}>
-            <main className= "p-4 mt-2 md:mt-14 w-full max-w-2xl" >
+            <main className="p-4 mt-2 md:mt-14 w-full max-w-2xl" >
                 <div>
                     authorization
                 </div>
